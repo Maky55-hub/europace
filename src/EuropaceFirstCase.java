05aeb77add2943e93ea58698e0c88ac8a8b4e7e7
Maky55-hub <57489140+Maky55-hub@users.noreply.github.com>
2021-04-14 13:34:31 +0200

    My first commit for europace project

---
+++ src/EuropaceFirstCase.java
@@ -0,0 +1,93 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class EuropaceFirstCase {
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new BufferedReader(new FileReader("resources/hypoport.json")));
+            JSONObject jsonObj = (JSONObject) obj;
+            JSONArray pricesArray = (JSONArray) jsonObj.get("prices");
+
+            // variables to find the days with smallest low and highest high
+            double smallestLow =  Double.parseDouble(((JSONObject) pricesArray.get(0)).get("low").toString());
+            double highestHigh = Double.parseDouble(((JSONObject) pricesArray.get(0)).get("high").toString());
+            String dayWithSmallestLow = ((JSONObject) pricesArray.get(0)).get("date").toString();
+            String dayWithHighestHigh = dayWithSmallestLow;
+
+            // variables to find the day with the largest difference between open and close
+            double defaultOpen = Double.parseDouble(((JSONObject) pricesArray.get(0)).get("open").toString());
+            double defaultClose = Double.parseDouble(((JSONObject) pricesArray.get(0)).get("close").toString());
+            double defaultDifferenceOpenClose = Math.abs(defaultOpen - defaultClose);
+            String dayWithLargestDifference = dayWithSmallestLow;
+
+            double sumOfAllCloses = 0;
+
+            for (Object dayPrice : pricesArray) {
+                JSONObject singleDay = (JSONObject) dayPrice;
+
+                Object high = singleDay.get("high");
+                Object low = singleDay.get("low");
+                Object open = singleDay.get("open");
+                Object close = singleDay.get("close");
+                String singleDayDate = singleDay.get("date").toString();
+
+                // code to find the day with smallest low
+                if ( low != null ) {
+                    double singleDayLow = Double.parseDouble(low.toString());
+                    if (singleDayLow < smallestLow) {
+                        smallestLow = singleDayLow;
+                        dayWithSmallestLow = singleDayDate;
+                    }
+                }
+
+                // code to find the highest high
+                if ( high != null) {
+                    double singleDayHigh = Double.parseDouble(high.toString());
+                    if (singleDayHigh > highestHigh) {
+                        highestHigh = singleDayHigh;
+                        dayWithHighestHigh = singleDayDate;
+                    }
+                }
+
+
+                // code to find the day with largest difference between open and close
+                if ( open != null && close != null ) {
+                    double singleDayOpen = Double.parseDouble(singleDay.get("open").toString());
+                    double singleDayClose = Double.parseDouble(singleDay.get("close").toString());
+                    double differenceOpenClose = Math.abs(singleDayOpen - singleDayClose);
+
+                    if (differenceOpenClose > defaultDifferenceOpenClose) {
+                        defaultDifferenceOpenClose = differenceOpenClose;
+                        dayWithLargestDifference = singleDayDate;
+                    }
+                }
+
+                // code to sum up closes and find average close
+                if ( close != null ) {
+                    sumOfAllCloses += Double.parseDouble(close.toString());
+                }
+
+            }
+
+            double averageOfAllCloses = sumOfAllCloses / pricesArray.size();
+
+            System.out.println("Day with smallest low: "+dayWithSmallestLow+". The smallest low is: "+smallestLow);
+            System.out.println("Day with the highest high: "+dayWithHighestHigh+". The highest high is: "+highestHigh);
+            System.out.println("Day with the largest difference between open and close: "+dayWithLargestDifference+". The differece is: "+defaultDifferenceOpenClose);
+            System.out.println("Average of all closes: "+ averageOfAllCloses);
+
+        } catch (ParseException | FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
